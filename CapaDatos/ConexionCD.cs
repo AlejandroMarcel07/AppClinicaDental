e43a89833a9fc392f1394b84ccf89728afa83755
marcel zuniga <alejocrak321zuniga@gmail.com>
2024-04-29 07:45:37 -0600

    Boton de expandir agregado, capas agregada(clase de conexion agregada)

---
+++ CapaDatos/ConexionCD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ConexionCD
+    {
+        string CadenaConexion = "Server = DESKTOP-E77K5U7\\SQLEXPRESS; Database= NombreBaseDeDatos; Integrated Security = True";
+
+        //Demas codigo
+    }
+}
--- CapaPresentacion/PantallaPrincipal.xaml.cs
+++ CapaPresentacion/PantallaPrincipal.xaml.cs
@@ -118,20 +118,21 @@ namespace CapaPresentacion
             MostrarPagina(new Page5());
         }
 
-        //private void btnExpandir_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (this.WindowState == WindowState.Normal)
-        //    {
+        private void btnExpandir_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
 
-        //        BordePrincipalSombra.Margin = new Thickness(0);
-        //        this.WindowState = WindowState.Maximized;
+                BordePrincipalSombra.Margin = new Thickness(0);
+                this.WindowState = WindowState.Maximized;
 
-        //    }
-        //    else {
-        //        BordePrincipalSombra.Margin = new Thickness(30);
-        //        this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                BordePrincipalSombra.Margin = new Thickness(30);
+                this.WindowState = WindowState.Normal;
 
-        //    }
-        //}
+            }
+        }
     }
 }
